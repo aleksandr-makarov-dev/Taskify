35e51a23a5225ec8898ad0a9695f714276798087
Aleksandr <alexandr.makarov.2000@gmail.com>
2026-09-19 19:30:56 +0300

    chore: fix ValidateAttribute

--- Taskify.WebApi/Infrastructure/Filters/ValidateAttribute.cs
+++ Taskify.WebApi/Infrastructure/Filters/ValidateAttribute.cs
@@ -3,4 +3,5 @@ using Microsoft.AspNetCore.Mvc;
 namespace Taskify.WebApi.Infrastructure.Filters;
 
 [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
-public sealed class ValidateAttribute(Type type) : TypeFilterAttribute(type);
+public sealed class ValidateAttribute(Type type)
+    : TypeFilterAttribute(typeof(ValidationFilter<>).MakeGenericType(type));
